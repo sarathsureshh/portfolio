75b4bcf72f4f947936d8a76d53f652555c8e882a
Sarath Suresh <sarathsuresh9360@gmail.com>
2023-11-19 22:57:30 +0530

    SEO fix

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -12,12 +12,9 @@ const SEO: FC<SEOProps> = ({ pageTitle, pageDescription }) => {
       <Head>
         <title>{pageTitle}</title>
         <meta name="description" content={pageDescription} />
-        <meta property="og:title" content="My Clothing Store" />
-        <meta
-          property="og:description"
-          content="Come to my store for great apparel!"
-        />
-        <meta property="og:url" content="https://shemilkumar.com/" />
+        <meta property="og:title" content="Sarath's portfolio" />
+        <meta property="og:description" content="Checkout sarath's portfolio" />
+        <meta property="og:url" content="https://sarathsureshh.vercel.app/" />
         <meta property="og:type" content="website" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
